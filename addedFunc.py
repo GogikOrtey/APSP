0682d1de88c4b0d63a39fa0c74e403854e4fba45
Georgiy Orlov <orlov-ga@brandpolgroup.com>
2025-10-17 16:34:34 +0500

    Сделал красивое форматирование запросов и ответов

--- addedFunc.py
+++ addedFunc.py
@@ -1,2 +1,65 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+# Отправляет запрос к YandexGPT и возвращает текстовый ответ модели.
+def sendMessageToYandexGPT(prompt: str, isPrint: bool = False, isSmartModel: bool = False) -> str:
+    print("Посылаю запрос к YandexGPT:")
+    if isPrint:
+            print(f"\n💫 PROMPT:\n{prompt}\n")
+    if isSmartModel:
+        model = "yandexgpt"
+        # model = "yandexgpt-pro" # Не работает
+    else:
+        model = "yandexgpt-lite" # Используется по умолчанию
+
+    load_dotenv()  # Загружаем .env с токеном API
+
+    api_key = os.getenv("API_KEY")
+    folder_id = "b1gomnf0uf3ims6eha2v"
+
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Authorization": f"Api-Key {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "modelUri": f"gpt://{folder_id}/{model}/latest",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokens": 200 ### Обратить внимание, на будущее
+        },
+        "messages": [
+            {"role": "user", "text": prompt}
+        ]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.ok:
+        result = response.json()
+        result_text = result["result"]["alternatives"][0]["message"]["text"]
+        # print_result_text = formatted_json_prompt_and_answer(prompt, result_text)
+        # if(isPrint): print("\n" + print_result_text + "\n") 
+        # Потом можно будет вывести в словарь вместе с ответом
+        if isPrint:
+            # print(f"\n💫 PROMPT:\n{prompt}\n\n💬 AI ANSWER:\n{result_text}\n")
+            print(f"\n💬 AI ANSWER:\n{result_text}\n")
+        return result_text
+    else:
+        raise Exception(f"Ошибка {response.status_code}: {response.text}")
+    
+def formatted_json_prompt_and_answer(prompt, answer):
+    data = {
+        "prompt": prompt,
+        "answer": answer
+    }
+    # И наверное надо будет добавлять что-то типо кода, или порядкового номера запроса. Но это позже
+    return json.dumps(data, ensure_ascii=False, indent=4)
+
+
 def clearAnswerCode(input_code):
     return input_code
--- mainTest_1.py
+++ mainTest_1.py
@@ -1,15 +1,15 @@
-from sendMessageToYandexGPT import send_to_yandex_gpt
-from sendMessageToYandexGPT import clearAnswerCode
+from addedFunc import sendMessageToYandexGPT
+from addedFunc import clearAnswerCode
 
 # Обозначения переменных (буду сокращать для удобства):
 # [описание запроса]_p1 = [описание запроса] prompt 1
 # a_test_p1 = answer test prompt 1
 
 test_p1 = "Напиши код сортировки пузырьком на питоне"
-a_test_p1 = send_to_yandex_gpt(test_p1, True, True)
+a_test_p1 = sendMessageToYandexGPT(test_p1, True, False)
 # print("Ответ от YandexGPT:")
 # print(answer)
 
-print(clearAnswerCode(a_test_p1))
+# print(clearAnswerCode(a_test_p1))
 
 
