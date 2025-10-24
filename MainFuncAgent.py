d4392e897d7dd1cc4a4cb2333d8360a64d41fe61
Georgiy Orlov <orlov-ga@brandpolgroup.com>
2025-10-24 17:52:05 +0500

    Очень короткие и интересные селекторы. Надо будет подробнее посмотреть на их корректность

--- MainFuncAgent.py
+++ MainFuncAgent.py
@@ -57,108 +57,110 @@ isPrint = False
 
 
 
-# Данные извлечённые из таблицы, например:
-data_input_table = {
-    "links": {
-        "simple": [
-            # {
-            #     "link": "https://vodomirural.ru/catalog/vanny_stalnye_i_aksessuary_k_nim/33951/",
-            #     "name": "Ванна сталь 1600х700х400мм antika белый в комплекте с ножками ВИЗ в Екатеринбурге",
-            #     "price": "10 320",
-            #     "brand": "Аntika",
-            #     "stock": "В наличии",
-            #     "imageLink": ""
-            # },
-            {
-                "link": "https://vodomirural.ru/catalog/opora_klipsa/35508/",
-                "name": "Опора ППРС D25 в Екатеринбурге",
-                "price": "5",
-                "brand": "",
-                "stock": "В наличии",
-                "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/168/16809d1e998be5e9c79c5d78e3e2f659.webp"
-            },
-            {
-                "link": "https://vodomirural.ru/catalog/zaglushka/35457/",
-                "name": "Заглушка (D20) в Екатеринбурге",
-                "price": "4",
-                "brand": "MeerPlast",
-                "stock": "В наличии",
-                "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/246/246a504d1f7b2f5b10645bb86c8060c3.webp"
-            },
-            {
-                "link": "https://vodomirural.ru/catalog/krestovina/35188/",
-                "name": "Крестовина 20 ППРС в Екатеринбурге",
-                "price": "16",
-                "brand": "MeerPlast",
-                "stock": "В наличии",
-                "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/39f/39f1c40fccd66173cf21a1b847baa335.webp"
-            },
-            {
-                "link": "https://vodomirural.ru/catalog/mufta_kombinirovannaya_amerikanka_razemnaya_vn_rez/32506/",
-                "name": "Муфта комб. раз. ППРС (вн. рез.) 20-1/2 в Екатеринбурге",
-                "price": "102",
-                "brand": "MeerPlast",
-                "stock": "В наличии",
-                "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/1b4/1b42d7577c23ed7541f61b721e4fa018.webp"
-            }
-        ]
-    },
-    "search_requests": []
-}
-
-
-
-
-# # Данные с сайта 2
+# # Данные извлечённые из таблицы, например:
 # data_input_table = {
 #     "links": {
 #         "simple": [
+#             # {
+#             #     "link": "https://vodomirural.ru/catalog/vanny_stalnye_i_aksessuary_k_nim/33951/",
+#             #     "name": "Ванна сталь 1600х700х400мм antika белый в комплекте с ножками ВИЗ в Екатеринбурге",
+#             #     "price": "10 320",
+#             #     "brand": "Аntika",
+#             #     "stock": "В наличии",
+#             #     "imageLink": ""
+#             # },
 #             {
-#                 "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-aura-170-b530df76.html",
-#                 "name": "Акриловая ванна Triton Аура 170x70 (КОПЛЕКТ ванна,экрас,каркас) TRITON",
-#                 "price": "16 125",
-#                 "article": "00017728",
-#                 "brand": "TRITON",
-#                 # "stock": "В наличии",
-#                 "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/b530df7630d011ec812be0d55e0811bb_b530df7730d011ec812be0d55e0811bb.jpg"
+#                 "link": "https://vodomirural.ru/catalog/opora_klipsa/35508/",
+#                 "name": "Опора ППРС D25 в Екатеринбурге",
+#                 "price": "5",
+#                 "brand": "",
+#                 "stock": "В наличии",
+#                 "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/168/16809d1e998be5e9c79c5d78e3e2f659.webp"
 #             },
 #             {
-#                 "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-standart-160h70-ekstra-akril-cd18e8d4.html",
-#                 "name": "Акриловая ванна Triton Стандарт 160х70 Экстра TRITON",
-#                 "price": "9 900 руб.",
-#                 "article": "УТ000001951",
-#                 "brand": "TRITON",
-#                 "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/b530df7630d011ec812be0d55e0811bb_b530df7730d011ec812be0d55e0811bb.jpg"
+#                 "link": "https://vodomirural.ru/catalog/zaglushka/35457/",
+#                 "name": "Заглушка (D20) в Екатеринбурге",
+#                 "price": "4",
+#                 "brand": "MeerPlast",
+#                 "stock": "В наличии",
+#                 "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/246/246a504d1f7b2f5b10645bb86c8060c3.webp"
 #             },
 #             {
-#                 "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-standart-130-ekstra-akril-9767a71b.html",
-#                 "name": "Акриловая ванна Triton Стандарт 130х70 Экстра TRITON",
-#                 "price": "7 990 руб.",
-#                 "article": "УТ000006868",
-#                 "brand": "TRITON",
-#                 "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/cd18e8d400d511e38427001a4d504e55_97912f653b7d11ea80e8e0d55e0811bb.jpg"
+#                 "link": "https://vodomirural.ru/catalog/krestovina/35188/",
+#                 "name": "Крестовина 20 ППРС в Екатеринбурге",
+#                 "price": "16",
+#                 "brand": "MeerPlast",
+#                 "stock": "В наличии",
+#                 "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/39f/39f1c40fccd66173cf21a1b847baa335.webp"
 #             },
 #             {
-#                 "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-izabel-pravaya-1700x1000-mm-fb2cccfd.html",
-#                 "name": "Акриловая ванна Triton Изабель 170х100 R TRITON",
-#                 "price": "24 820 руб.",
-#                 "article": "УТ000001271",
-#                 "brand": "TRITON",
-#                 "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/fb2cccfd42b211e2859e001a4d504e55_04a3a1a4eb5b11ee8148e0d55e0811bb.jpg"
-#             },
-#             {
-#                 "link": "https://santehnica-vodoley.ru/catalog/kotelnoe-oborudovanie/komplektuyucshie-dlya-kotelnogo-oborudovaniya/prokladka-iz-ftoroplasta-34-MasterProf-58316128.html",
-#                 "name": "Прокладка из фторопласта 3/4\" MasterProf MasterProf",
-#                 "price": "15 руб.",
-#                 "article": "00027670",
-#                 "brand": "MasterProf",
-#                 # Не в наличии
-#                 "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/583161280d2a11ef814ae0d55e0811bb_5831613b0d2a11ef814ae0d55e0811bb.jpg"
+#                 "link": "https://vodomirural.ru/catalog/mufta_kombinirovannaya_amerikanka_razemnaya_vn_rez/32506/",
+#                 "name": "Муфта комб. раз. ППРС (вн. рез.) 20-1/2 в Екатеринбурге",
+#                 "price": "102",
+#                 "brand": "MeerPlast",
+#                 "stock": "В наличии",
+#                 "imageLink": "https://vodomirural.ru/upload/resize_cache/webp/iblock/1b4/1b42d7577c23ed7541f61b721e4fa018.webp"
 #             }
 #         ]
 #     },
 #     "search_requests": []
 # }
+
+
+
+############################## article не нашёл !!!
+
+
+# Данные с сайта 2
+data_input_table = {
+    "links": {
+        "simple": [
+            {
+                "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-aura-170-b530df76.html",
+                "name": "Акриловая ванна Triton Аура 170x70 (КОПЛЕКТ ванна,экрас,каркас) TRITON",
+                "price": "16 125",
+                "article": "00017728",
+                "brand": "TRITON",
+                # "stock": "В наличии",
+                "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/b530df7630d011ec812be0d55e0811bb_b530df7730d011ec812be0d55e0811bb.jpg"
+            },
+            {
+                "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-standart-160h70-ekstra-akril-cd18e8d4.html",
+                "name": "Акриловая ванна Triton Стандарт 160х70 Экстра TRITON",
+                "price": "9 900 руб.",
+                "article": "УТ000001951",
+                "brand": "TRITON",
+                "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/b530df7630d011ec812be0d55e0811bb_b530df7730d011ec812be0d55e0811bb.jpg"
+            },
+            {
+                "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-standart-130-ekstra-akril-9767a71b.html",
+                "name": "Акриловая ванна Triton Стандарт 130х70 Экстра TRITON",
+                "price": "7 990 руб.",
+                "article": "УТ000006868",
+                "brand": "TRITON",
+                "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/cd18e8d400d511e38427001a4d504e55_97912f653b7d11ea80e8e0d55e0811bb.jpg"
+            },
+            {
+                "link": "https://santehnica-vodoley.ru/catalog/vanny/vanny-akrilovye/vanna-izabel-pravaya-1700x1000-mm-fb2cccfd.html",
+                "name": "Акриловая ванна Triton Изабель 170х100 R TRITON",
+                "price": "24 820 руб.",
+                "article": "УТ000001271",
+                "brand": "TRITON",
+                "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/fb2cccfd42b211e2859e001a4d504e55_04a3a1a4eb5b11ee8148e0d55e0811bb.jpg"
+            },
+            {
+                "link": "https://santehnica-vodoley.ru/catalog/kotelnoe-oborudovanie/komplektuyucshie-dlya-kotelnogo-oborudovaniya/prokladka-iz-ftoroplasta-34-MasterProf-58316128.html",
+                "name": "Прокладка из фторопласта 3/4\" MasterProf MasterProf",
+                "price": "15 руб.",
+                "article": "00027670",
+                "brand": "MasterProf",
+                # Не в наличии
+                "imageLink": "https://santehnica-vodoley.ru/a/vodolei1/files/userfiles/images/catalog/583161280d2a11ef814ae0d55e0811bb_5831613b0d2a11ef814ae0d55e0811bb.jpg"
+            }
+        ]
+    },
+    "search_requests": []
+}
 
 
 
